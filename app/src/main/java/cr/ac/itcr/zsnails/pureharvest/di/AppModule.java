5817291e414062014e07047f709b4ba68482d6a0
zSnails <erizojuan33@gmail.com>
2025-03-29 23:50:05 -0600

    feat: add shopping cart repository provider

--- app/src/main/java/cr/ac/itcr/zsnails/pureharvest/di/AppModule.java
+++ app/src/main/java/cr/ac/itcr/zsnails/pureharvest/di/AppModule.java
@@ -2,6 +2,7 @@ package cr.ac.itcr.zsnails.pureharvest.di;
 
 import android.content.Context;
 
+import androidx.lifecycle.ViewModelProvider;
 import androidx.room.Room;
 
 import java.util.concurrent.ExecutorService;
@@ -10,6 +11,8 @@ import java.util.concurrent.Executors;
 import javax.inject.Singleton;
 
 import cr.ac.itcr.zsnails.pureharvest.domain.LocalCartDatabase;
+import cr.ac.itcr.zsnails.pureharvest.domain.repository.ShoppingCartRepository;
+import cr.ac.itcr.zsnails.pureharvest.ui.cart.ShoppingCartViewModel;
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
@@ -29,5 +32,11 @@ public class AppModule {
     @Singleton
     public ExecutorService provideExecutorService() {
         return Executors.newFixedThreadPool(5);
+    }
+
+    @Provides
+    @Singleton
+    public ShoppingCartRepository provideShoppingCartRepository(final LocalCartDatabase db) {
+        return new ShoppingCartRepository(db);
     }
 }
---
+++ app/src/main/java/cr/ac/itcr/zsnails/pureharvest/domain/repository/ShoppingCartRepository.java
@@ -0,0 +1,41 @@
+package cr.ac.itcr.zsnails.pureharvest.domain.repository;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import cr.ac.itcr.zsnails.pureharvest.dao.CartDao;
+import cr.ac.itcr.zsnails.pureharvest.domain.LocalCartDatabase;
+import cr.ac.itcr.zsnails.pureharvest.entities.CartItem;
+
+public class ShoppingCartRepository {
+
+    private final CartDao dao;
+
+    @Inject
+    public ShoppingCartRepository(
+            @NonNull final LocalCartDatabase db
+    ) {
+        this.dao = db.cartDao();
+    }
+
+    public void deleteAll() {
+        dao.deleteAll();
+    }
+
+    public void deleteById(Integer id) {
+        dao.deleteById(id);
+    }
+
+    public void insert(CartItem... items) {
+        dao.insertAll(items);
+    }
+
+    public List<CartItem> all() {
+        return dao.getAll();
+    }
+
+
+}
