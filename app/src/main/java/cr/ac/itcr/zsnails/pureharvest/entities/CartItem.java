27930fdab4eebc0ba2537983b4435bb2e0663f8a
zSnails <erizojuan33@gmail.com>
2025-03-29 21:51:19 -0600

    fix: add missing amount field to cart item class

--- app/src/main/java/cr/ac/itcr/zsnails/pureharvest/entities/CartItem.java
+++ app/src/main/java/cr/ac/itcr/zsnails/pureharvest/entities/CartItem.java
@@ -1,15 +1,27 @@
 package cr.ac.itcr.zsnails.pureharvest.entities;
 
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "cart")
 public class CartItem {
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     public long id;
 
     @ColumnInfo(name = "product_id")
     public String productId;
+
+    @ColumnInfo(name = "amount")
+    public Integer amount;
+
+    public CartItem() {
+    }
+
+    public CartItem(@NonNull final String productId, final Integer amount) {
+        this.productId = productId;
+        this.amount = amount;
+    }
 }
