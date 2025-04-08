d609892a386407fd4703d14a2f89c1e0753d2d15
JossephVR <114211961+JossephVR@users.noreply.github.com>
2025-04-08 16:56:34 -0600

    Feat: Modifiable company data in database

--- app/src/main/java/cr/ac/itcr/zsnails/pureharvest/ui/Profile/EditProfileFragment.java
+++ app/src/main/java/cr/ac/itcr/zsnails/pureharvest/ui/Profile/EditProfileFragment.java
@@ -7,16 +7,80 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import cr.ac.itcr.zsnails.pureharvest.R;
+import cr.ac.itcr.zsnails.pureharvest.databinding.FragmentEditProfileBinding;
 
 public class EditProfileFragment extends Fragment {
 
+    private FragmentEditProfileBinding binding;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        binding = FragmentEditProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+
+        EditText nameInput = root.findViewById(R.id.nameInput);
+        EditText sloganInput = root.findViewById(R.id.sloganInput);
+        EditText numberInput = root.findViewById(R.id.numberInput);
+        EditText emailInput = root.findViewById(R.id.emailInput);
+        EditText adressInput = root.findViewById(R.id.adressInput);
+        EditText mapAdressInput = root.findViewById(R.id.mapAdressInput);
+        Button saveChangesBtn = root.findViewById(R.id.saveChangesBtn);
+
+
+        db.collection("Company").document("1").get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if(documentSnapshot.exists()){
+                        nameInput.setText(documentSnapshot.getString("name"));
+                        sloganInput.setText(documentSnapshot.getString("slogan"));
+                        numberInput.setText(documentSnapshot.getString("number"));
+                        emailInput.setText(documentSnapshot.getString("email"));
+                        adressInput.setText(documentSnapshot.getString("adress"));
+                        mapAdressInput.setText(documentSnapshot.getString("mapAdress"));
+                    }
+                });
+
+
+
+        saveChangesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name = nameInput.getText().toString();
+                String slogan = sloganInput.getText().toString();
+                String number = numberInput.getText().toString();
+                String email = emailInput.getText().toString();
+                String adress = adressInput.getText().toString();
+                String mapAdress = mapAdressInput.getText().toString();
+
+                Map<String, Object> companyData = new HashMap<>();
+
+                companyData.put("name", name);
+                companyData.put("slogan", slogan);
+                companyData.put("number", number);
+                companyData.put("email", email);
+                companyData.put("adress", adress);
+                companyData.put("mapAdress", mapAdress);
+
+                db.collection("Company").document("1").set(companyData);
+
+            }
+        });
+
+        return root;
     }
 }
--- app/src/main/java/cr/ac/itcr/zsnails/pureharvest/ui/Profile/ProfileFragment.java
+++ app/src/main/java/cr/ac/itcr/zsnails/pureharvest/ui/Profile/ProfileFragment.java
@@ -9,6 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import cr.ac.itcr.zsnails.pureharvest.R;
 import cr.ac.itcr.zsnails.pureharvest.databinding.FragmentAccountBinding;
@@ -19,11 +22,34 @@ public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
 
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        TextView nameT = root.findViewById(R.id.NameT);
+        TextView sloganT = root.findViewById(R.id.sloganT);
+        TextView phoneT = root.findViewById(R.id.phoneT);
+        TextView emailT = root.findViewById(R.id.emailT);
+        TextView adressT = root.findViewById(R.id.adressT);
+        TextView mapAdressT = root.findViewById(R.id.mapAdressT);
+
+        db.collection("Company").document("1").get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if(documentSnapshot.exists()){
+                        nameT.setText(documentSnapshot.getString("name"));
+                        sloganT.setText(documentSnapshot.getString("slogan"));
+                        phoneT.setText(documentSnapshot.getString("number"));
+                        emailT.setText(documentSnapshot.getString("email"));
+                        adressT.setText(documentSnapshot.getString("adress"));
+                        mapAdressT.setText(documentSnapshot.getString("mapAdress"));
+                    }
+                });
 
         Button editProfileBtn = root.findViewById(R.id.editProfileBtn);
 
