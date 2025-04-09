eeb751332e399fe7868e2b4eccf1f375b09536e7
zSnails <erizojuan33@gmail.com>
2025-04-09 13:22:04 -0600

    refactor: the shopping cart adapter now accepts the Item interface instead of the CartItem class

--- app/src/main/java/cr/ac/itcr/zsnails/pureharvest/ui/cart/adapter/ShoppingCartAdapter.java
+++ app/src/main/java/cr/ac/itcr/zsnails/pureharvest/ui/cart/adapter/ShoppingCartAdapter.java
@@ -9,23 +9,26 @@ import androidx.recyclerview.widget.RecyclerView.Adapter;
 import java.util.ArrayList;
 import java.util.List;
 
-import cr.ac.itcr.zsnails.pureharvest.entities.CartItem;
+import cr.ac.itcr.zsnails.pureharvest.ui.cart.Item;
+import cr.ac.itcr.zsnails.pureharvest.ui.cart.ShoppingCartViewModel;
 
 
-public final class ShoppingCartAdapter extends Adapter<Card> {
-    private List<CartItem> items = new ArrayList<>();
+public final class ShoppingCartAdapter extends Adapter<Card> implements ShoppingCartViewModel.ItemOperationEventListener {
+    private final Card.AmountTapListener amountTapListener;
+    private List<Item> items = new ArrayList<>();
 
-    public ShoppingCartAdapter() {
+    public ShoppingCartAdapter(Card.AmountTapListener listener) {
         setHasStableIds(true);
+        this.amountTapListener = listener;
     }
 
     @Override
     public long getItemId(int position) {
-        return items.get(position).id;
+        return items.get(position).getId();
     }
 
     @SuppressLint("NotifyDataSetChanged")
-    public void setItems(List<CartItem> items) {
+    public void setItems(List<Item> items) {
         this.items = items;
         notifyDataSetChanged();
     }
@@ -34,24 +37,20 @@ public final class ShoppingCartAdapter extends Adapter<Card> {
     @Override
     public Card onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Card card = Card.from(parent);
+        card.setAmountTapListener(amountTapListener);
         return card;
     }
 
     public void removeAt(Integer idx) {
-        CartItem item = items.get(idx);
+        Item item = items.get(idx);
         items.remove(item);
         notifyItemRemoved(idx);
     }
 
     @Override
     public void onBindViewHolder(@NonNull Card holder, int position) {
-        // TODO: show shopping cart data, the user does not yet have shopping cart information here
-        // here I won't be using this, but the actual bind method I defined in the Card view holder
-        CartItem current = items.get(position);
-        holder.binding.productName.setText(current.productId);
-        if (current.amount != null) {
-            holder.binding.cartItemAmount.setText(current.amount.toString());
-        }
+        Item current = items.get(position);
+        holder.bind(current);
     }
 
     @Override
@@ -63,6 +62,21 @@ public final class ShoppingCartAdapter extends Adapter<Card> {
     public void onViewRecycled(@NonNull Card holder) {
         super.onViewRecycled(holder);
         holder.setState(new CardClosedState(holder));
+    }
+
+    @Override
+    public void onItemCreated(Item item) {
+    }
+
+    @Override
+    public void onItemRemoved(Item item, int position) {
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public void onItemUpdated(Item item) {
+        int idx = this.items.indexOf(item);
+        notifyItemChanged(idx, item);
     }
 }
 
