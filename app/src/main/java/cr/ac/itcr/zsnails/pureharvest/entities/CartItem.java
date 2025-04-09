82cb5e8962299c22243b5b20ee4f996cc9ec6f4d
zSnails <erizojuan33@gmail.com>
2025-04-09 12:43:51 -0600

    refactor: CartItem now implements the Item interface

--- app/src/main/java/cr/ac/itcr/zsnails/pureharvest/entities/CartItem.java
+++ app/src/main/java/cr/ac/itcr/zsnails/pureharvest/entities/CartItem.java
@@ -6,8 +6,12 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.io.Serializable;
+
+import cr.ac.itcr.zsnails.pureharvest.ui.cart.Item;
+
 @Entity(tableName = "cart")
-public class CartItem {
+public class CartItem implements Item, Serializable {
     @PrimaryKey(autoGenerate = true)
     public long id;
 
@@ -22,6 +26,41 @@ public class CartItem {
 
     public CartItem(@NonNull final String productId, final Integer amount) {
         this.productId = productId;
+        this.amount = amount;
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String getProductId() {
+        return productId;
+    }
+
+    @Override
+    public String getName() {
+        return "mock name";
+    }
+
+    @Override
+    public Double getPrice() {
+        return 16.53;
+    }
+
+    @Override
+    public String getType() {
+        return "mock type";
+    }
+
+    @Override
+    public Integer getAmount() {
+        return amount;
+    }
+
+    @Override
+    public void setAmount(Integer amount) {
         this.amount = amount;
     }
 }
