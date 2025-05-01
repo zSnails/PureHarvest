0c53dd45dc83bc9c45fa6809d00ebc256fead42e
GabrielAlfaro07 <galfaulate@gmail.com>
2025-05-01 01:20:22 -0600

    fix: adapted UI according to Figma prototype

--- app/src/main/java/cr/ac/itcr/zsnails/pureharvest/ui/home/ProductAdapter.java
+++ app/src/main/java/cr/ac/itcr/zsnails/pureharvest/ui/home/ProductAdapter.java
@@ -5,9 +5,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.bumptech.glide.Glide;
 import cr.ac.itcr.zsnails.pureharvest.R;
+
 import java.util.List;
 
 import cr.ac.itcr.zsnails.pureharvest.data.model.Product;
@@ -25,20 +29,21 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
         notifyDataSetChanged();
     }
 
+    @NonNull
     @Override
-    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_product, parent, false);
         return new ProductViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(ProductViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
         holder.bind(products.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return products.size();
+        return products != null ? products.size() : 0;
     }
 
     public static class ProductViewHolder extends RecyclerView.ViewHolder {
@@ -52,11 +57,20 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
             productImage = itemView.findViewById(R.id.productImage);
             productName = itemView.findViewById(R.id.productName);
             productPrice = itemView.findViewById(R.id.productPrice);
+            ImageView addToCart = itemView.findViewById(R.id.addToCartButton);
+
+            addToCart.setOnClickListener(v -> {
+                // TODO: Implement Add to Cart action
+            });
+
+            itemView.setOnClickListener(v -> {
+                // TODO: Navigate to product detail activity
+            });
         }
 
         public void bind(Product product) {
             productName.setText(product.getName());
-            productPrice.setText("₡" + (Double) product.getPrice()); // Costa Rican Colón
+            productPrice.setText("₡" + product.getPrice());
             Glide.with(productImage.getContext()).load(product.getFirstImageUrl()).into(productImage);
         }
     }
