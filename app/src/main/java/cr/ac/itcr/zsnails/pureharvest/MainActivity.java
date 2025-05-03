2325d6f3328ea0ec2bbf49d3a67681f809501a06
JossephVR <114211961+JossephVR@users.noreply.github.com>
2025-05-03 14:55:36 -0600

    Fix: go back button fixed

--- app/src/main/java/cr/ac/itcr/zsnails/pureharvest/MainActivity.java
+++ app/src/main/java/cr/ac/itcr/zsnails/pureharvest/MainActivity.java
@@ -45,4 +45,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        return navController.navigateUp() ||super.onSupportNavigateUp();
+    }
 }
