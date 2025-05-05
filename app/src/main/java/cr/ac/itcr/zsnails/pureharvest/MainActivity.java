d4ee8a0e267a36bca0c7fc05114f06af21a7f1a7
FabsCR <fabianfdez06@gmail.com>
2025-05-05 01:59:16 -0600

    refactor: replaced HomeFragment buttons with global FAB in MainActivity

--- app/src/main/java/cr/ac/itcr/zsnails/pureharvest/MainActivity.java
+++ app/src/main/java/cr/ac/itcr/zsnails/pureharvest/MainActivity.java
@@ -9,6 +9,9 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import android.content.Intent;
+import android.widget.PopupMenu;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import cr.ac.itcr.zsnails.pureharvest.databinding.ActivityMainBinding;
 
@@ -24,14 +27,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_account)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-    }
 
+        binding.fabMenu.setOnClickListener(view -> {
+            PopupMenu popup = new PopupMenu(MainActivity.this, view);
+            popup.getMenuInflater().inflate(R.menu.fab_menu, popup.getMenu());
+
+            popup.setOnMenuItemClickListener(item -> {
+                if (item.getItemId() == R.id.action_create_product) {
+                    startActivity(new Intent(MainActivity.this, cr.ac.itcr.zsnails.pureharvest.ui.seller.CreateProductActivity.class));
+                    return true;
+                } else if (item.getItemId() == R.id.action_view_product) {
+                    startActivity(new Intent(MainActivity.this, cr.ac.itcr.zsnails.pureharvest.ui.client.ViewProductActivity.class));
+                    return true;
+                }
+                return false;
+            });
+
+            popup.show();
+        });
+    }
 }
--- app/src/main/java/cr/ac/itcr/zsnails/pureharvest/ui/home/HomeFragment.java
+++ app/src/main/java/cr/ac/itcr/zsnails/pureharvest/ui/home/HomeFragment.java
@@ -1,57 +1,29 @@
 package cr.ac.itcr.zsnails.pureharvest.ui.home;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import cr.ac.itcr.zsnails.pureharvest.databinding.FragmentHomeBinding;
-import cr.ac.itcr.zsnails.pureharvest.ui.client.ViewProductActivity;
-import cr.ac.itcr.zsnails.pureharvest.ui.seller.CreateProductActivity;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        // ViewModel initialization
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
-        // Inflate layout using View Binding
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        // Observe and display text from ViewModel
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-
-        // Button to open CreateProductActivity
-        binding.button.setOnClickListener(v -> {
-            Intent intent = new Intent(getActivity(), CreateProductActivity.class);
-            startActivity(intent);
-        });
-
-        // Button to open ViewProductActivity
-        binding.button2.setOnClickListener(v -> {
-            Intent intent = new Intent(getActivity(), ViewProductActivity.class);
-            startActivity(intent);
-        });
-
-        return root;
+        return binding.getRoot();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        // Clear binding reference to avoid memory leaks
         binding = null;
     }
 }
